f5b0ba32c2d955f3db312d64ebf12fb2424a0eae
nehachaudhari20 <chaudharineha2005@gmail.com>
2026-05-24 00:08:11 +0530

    New struct

---
+++ vitalops/src/test/java/com/vitalops/VitalopsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vitalops;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VitalopsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
